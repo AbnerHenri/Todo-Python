85a009d40386b9cee736441320c7a62559ef06e2
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-10-03 10:50:40 -0300

    Add Models

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,3 +1,18 @@
+from secrets import choice
+from telnetlib import STATUS
+from turtle import done
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+
+    STATUS = (
+        (1, 'Doing'),
+        (2, 'Done')
+    )
+
+    title = models.CharField(_MAX_LENGTH=255)
+    description = models.TextField()
+    done = models.CharField(_MAX_LENGTH=1, choices=STATUS)
